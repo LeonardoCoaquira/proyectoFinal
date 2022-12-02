2ac9bc1a0ef091a5effd5aae405d1c29e33441b1
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2022-12-02 14:18:05 -0500

    connection with mongodb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div class="col-md-2">    
             <div class="card">
-                <img src="{{$animal}}" class="rounded mx-auto d-block" alt="...">
+                <img src="{{$animal}}" class="rounded mx-auto d-block" alt="">
             </div>
         </div>
     </div>
